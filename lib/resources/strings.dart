5199807871669bb13f6bd74c7260c885cd2ebe15
Christian Neil Anthony Wico <cnawico@gmail.com>
2023-06-09 22:47:24 +0800

    Create strings.dart

---
+++ lib/resources/strings.dart
@@ -0,0 +1,5 @@
+class Strings {
+  static const appTitle = 'Flutter Technical Test Lev. 1';
+  static const widgetTestPageTitle = 'Widget Test Page';
+  static const logicTestPageTitle = 'Logic Test Page';
+}
